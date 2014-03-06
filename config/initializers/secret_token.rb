6943689e6a1b181ac64c1d815a01f72c479b9a19
Daniel McGraw <dan.j.mcgraw@gmail.com>
2014-03-05 20:52:05 -0700

    Start it up.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twentyterms::Application.config.secret_key_base = 'd93bd2ce935dd2e4323003624c9f620e5f369627960960c00f889cb8544f29e4ade2824b81e84ca7849b4e48cc114cef1fc30a6ef3eccdd4e7236e7f898be74e'
